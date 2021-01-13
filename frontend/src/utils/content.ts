7c912ddc00a73daf92ace657c1d651d1f18ae78b
Southclaws <accounts@southcla.ws>
2021-01-13 14:57:35 +0000

    update docs to reflect new category pages
    removed some unused index.md files
    
    renamed _tutorials.md to _.md so a proper category page is generated

--- frontend/src/utils/content.ts
+++ frontend/src/utils/content.ts
@@ -148,6 +148,9 @@ export const readLocaleDocs = async (
     return { source, fallback: true };
   }
 
+  // The path may not be a file, it may be a directory. If so, generate a page
+  // for this directory that lists all its children as well as any content in
+  // a special file named `_.md`.
   const dir = "../docs/" + fullName;
   if (statSync(dir).isDirectory()) {
     const list = readdirSync(dir);
@@ -161,8 +164,9 @@ export const readLocaleDocs = async (
     // Generate some content for this category page. A heading, some content
     // from the index.md if it exists and a list of pages inside it.
     const additional = flow(
-      filter((v: string) => v !== "index.md"), // filter out the special index page
-      map((v: string) => `- [${v.replace(".md", "")}](${name + "/" + v})`), // generate a ul element
+      filter((v: string) => v !== "_.md"), // filter out the special index page
+      map((v: string) => v.replace(".md", "")), // remove the file extension
+      map((v: string) => `- [${v}](${name + "/" + v})`), // generate a ul element
       ldjoin("\n")
     )(list);
 
